9e40c5f1cf4596604589f1949695cee42e246b32
Andra Markov <andramarkov@gmail.com>
2023-01-04 14:11:52 +0700

    pix

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wpbaru' );
+define( 'DB_NAME', 'wpbaru' ); //changeme
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'root' ); //changeme
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '' ); //changeme
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost' ); //changeme
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
